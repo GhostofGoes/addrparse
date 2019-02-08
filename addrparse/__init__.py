7be245fdd1bbb7c13db42035ec494c2bbd6e670f
Christopher Goes <ghostofgoes@gmail.com>
2019-02-08 11:51:25 -0700

    Initial commit

---
+++ addrparse/__init__.py
@@ -0,0 +1,6 @@
+"""The public API for addrparse.
+
+This is what you get if you do ``from addrparse import *``
+"""
+
+__version__ = '0.1.0'
---
+++ addrparse/core.py
@@ -0,0 +1,34 @@
+"""The core functionality for addrparse.
+
+Functions that take mixed input and spit out a normalized set.
+
+
+What's in mixed input:
+* Filenames: text file, JSON file, INI file?
+* CIDR-specified ranges
+* "nmap"-style ranges (e.g. 192.168.0.20-40, 172.16-17.0.0)
+* "verbose" ranges (e.g. 192.168.0.20-192.168.0.40)
+* Domain names
+* Plain jane IP addresses (IPv4 and IPv6!)
+
+"""
+
+import ipaddress
+import os
+import sys
+from pathlib import Path
+from typing import Set, Dict
+
+
+# TODO: python 3.6 only? (fstrings, better pathlib, variable annotations, etc.)
+def to_strings():
+    pass
+
+
+# returns set of IPAddress objects
+def to_objs():
+    pass
+
+
+
+
